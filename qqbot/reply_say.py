c6b58dcbf29f788730775307c2864745d235a55c
litufu <285212818@qq.com>
2019-05-28 14:52:28 +0800

    修改qq查询方式

--- qqbot/reply_say.py
+++ qqbot/reply_say.py
@@ -5,7 +5,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
 import json
+from selenium.webdriver.support.select import Select
 from multiprocessing import Pool
 from database import User, Base
 from session import session
@@ -17,7 +19,7 @@ class QQZoneSpider(object):
         self.password = password
         self.content = content
         chrome_options = Options()
-        chrome_options.add_argument('--headless')
+        # chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
         self.driver = webdriver.Chrome(
@@ -73,8 +75,52 @@ class QQZoneSpider(object):
         psw.clear()
         psw.send_keys(self.password)
         self.driver.find_element_by_id("login_button").click()
-        time.sleep(3)
+        time.sleep(30)
         self.save_cookie()
+
+    def get_code(self):
+        setting = self.driver.find_element_by_id('tb_setting_li')
+        actions = ActionChains(self.driver)
+        time.sleep(3)
+        actions.move_to_element(setting).perform()
+        time.sleep(3)
+        modify = self.driver.find_element_by_link_text('修改资料')
+        modify.click()
+        time.sleep(3)
+        self.driver.switch_to.frame('ttinfo')
+        county_select = self.driver.find_element_by_id('addslt_c_0')
+        county_list = county_select.find_elements_by_tag_name('option')
+        for option in county_list:
+            value = option.get_attribute("value")
+            text = option.text
+            print("Value is: " + value)
+            print("Text is:" + text)
+        Select(county_select).select_by_value("1")
+        time.sleep(3)
+        province_select = self.driver.find_element_by_id('addslt_s_0')
+        province_list = province_select.find_elements_by_tag_name('option')
+        result = []
+        for option in province_list:
+            province_value = option.get_attribute("value")
+            province_text = option.text
+            print("province Value is: " + province_value )
+            print("province Text is:" + province_text)
+            Select(province_select).select_by_value(province_value)
+            time.sleep(3)
+            city_select = self.driver.find_element_by_id('addslt_cty_0')
+            city_list = city_select.find_elements_by_tag_name('option')
+            cities = []
+            for city_option in city_list:
+                city_value = city_option.get_attribute("value")
+                city_text = city_option.text
+                cities.append({'code':city_value,'name':city_text})
+                print("city Value is: " + city_value)
+                print("city Text is:" + city_text)
+            result.append({"code": province_value, 'name': province_text, 'cities': cities})
+        file = open('area.json', 'w', encoding='utf-8')
+        print(result)
+        json.dump(result, file)
+        file.close()
 
     def reply(self, qq):
         self.driver.get(url='https://user.qzone.qq.com/{}'.format(qq))
@@ -126,10 +172,6 @@ def pool_send():
         'name': '3468350745',
         'password': "puDqax43LYwX9iC",
     },
-    #     {
-    #     'name': '2837484507',
-    #     'password': "UZ5tgxDVmuCxW58",
-    # },
     ]
     cnt = "高考志愿模拟填报选【格物水滴APP】,可即时查看你在所报考学校和专业的成绩排名，并且可以和所有报考者沟通交流。"
     length = len(senders)
@@ -146,15 +188,17 @@ def pool_send():
 if __name__ == '__main__':
     # pool_send()
     cnt = "高考志愿模拟填报选【格物水滴APP】,可即时查看你在所报考学校和专业的成绩排名，并且可以和所有报考者沟通交流。"
-    qqzone = QQZoneSpider(username="3468350745",password="puDqax43LYwX9iC",content=cnt)
-    users = session.query(User).filter(User.send == False).all()
-    for user in users:
-        try:
-            qqzone.reply(user.qq)
-        except Exception as e:
-            print(e)
-        user.send = True
-        session.commit()
-        time.sleep(2)
-        print('{}空间评价完成'.format(user.qq))
+    # qqzone = QQZoneSpider(username="3468350745",password="puDqax43LYwX9iC",content=cnt)
+    qqzone = QQZoneSpider(username="2837484507",password="UZ5tgxDVmuCxW58",content=cnt)
+    qqzone.get_code()
+    # users = session.query(User).filter(User.send == False).all()
+    # for user in users:
+    #     try:
+    #         qqzone.reply(user.qq)
+    #     except Exception as e:
+    #         print(e)
+    #     user.send = True
+    #     session.commit()
+    #     time.sleep(2)
+    #     print('{}空间评价完成'.format(user.qq))
 
